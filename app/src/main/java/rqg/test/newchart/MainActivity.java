ba9f2e1f11df97a8ae6b7815200be7e6a4e2dda2
rqg <380614540@qq.com>
2016-04-26 14:38:35 +0800

    commit

--- app/src/main/java/rqg/test/newchart/MainActivity.java
+++ app/src/main/java/rqg/test/newchart/MainActivity.java
@@ -1,7 +1,7 @@
 package rqg.test.newchart;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
